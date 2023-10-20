64cfe04e7f84f55f30d64053e26823dcc1d2b54d
mcdivine2 <markdave.canoy@sccpag.edu.ph>
2023-10-20 19:17:08 +0800

    with UserProfile Activity

--- app/src/main/java/com/example/carrental/DashBoard.kt
+++ app/src/main/java/com/example/carrental/DashBoard.kt
@@ -1,11 +1,21 @@
 package com.example.carrental
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class DashBoard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
+    }
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnUserProfile -> {
+                val intent = Intent(this, UserProfile::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
